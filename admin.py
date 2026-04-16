600fd595c6db805e68fc6889a29b0b12fa0fd3d9
Domastiano <96682299+Domastiano@users.noreply.github.com>
2026-04-16 14:20:44 +0200

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,5 +1,4 @@
-from django.contrib import admin
-from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission  # dodaj Submission
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -11,15 +10,15 @@ class QuestionInline(admin.StackedInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
+    list_display = ['content', 'course', 'grade']  # dodaj list_display
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title', 'order', 'course']
-    
-admin.site.register(Submission)
+    list_display = ['title', 'order', 'course']  # dodaj odpowiednie pola
+
 admin.site.register(Course)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
-admin.site.register(Submission)
+admin.site.register(Submission)  # dodaj rejestrację
