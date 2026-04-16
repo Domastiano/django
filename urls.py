d830c0df40f8c6c4467187370122c082b7577cf0
Domastiano <96682299+Domastiano@users.noreply.github.com>
2026-04-16 14:14:32 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,4 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'onlinecourse'
+
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:course_id>/', views.course_details, name='course_details'),
     path('<int:course_id>/submit/', views.submit, name='submit'),
     path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
 ]
