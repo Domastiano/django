43d25fe3bc631c73b285e4b679cae32a8ccc47eb
Domastiano <96682299+Domastiano@users.noreply.github.com>
2026-04-16 14:24:19 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:course_id>/', views.course_details, name='course_details'),
     path('<int:course_id>/submit/', views.submit, name='submit'),
-    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
+    path(
+        'course/<int:course_id>/submission/<int:submission_id>/result/',
+        views.show_exam_result,
+        name='show_exam_result'
+    ),
 ]
